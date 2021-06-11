7ba3c2ac982d45cd03b694620b92d72b92af13f4
rushmorem <rushmore@webenchanter.com>
2021-06-11 10:03:12 +0200

    make flake runnable

--- flake.nix
+++ flake.nix
@@ -1,35 +1,38 @@
 {
   description = "IPGen Command Line Tool";
 
-  inputs = {
-    import-cargo.url = github:edolstra/import-cargo;
-  };
+  inputs = { import-cargo.url = "github:edolstra/import-cargo"; };
 
-  outputs = { self, nixpkgs, import-cargo }: let
+  outputs = { self, nixpkgs, import-cargo }:
+    let
 
-    inherit (import-cargo.builders) importCargo;
+      inherit (import-cargo.builders) importCargo;
 
-  in {
+    in {
 
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "ipgen-cli";
-        src = self;
+      defaultPackage.x86_64-linux =
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation rec {
+          name = "ipgen";
+          src = self;
 
-        nativeBuildInputs = [
-          (importCargo { lockFile = ./Cargo.lock; inherit pkgs; }).cargoHome
+          nativeBuildInputs = [
+            (importCargo {
+              lockFile = ./Cargo.lock;
+              inherit pkgs;
+            }).cargoHome
 
-          rustc cargo
-        ];
+            rustc
+            cargo
+          ];
 
-        buildPhase = ''
-          cargo build --release --offline
-        '';
+          buildPhase = ''
+            cargo build --release --offline
+          '';
 
-        installPhase = ''
-          install -Dm775 ./target/release/ipgen $out/bin/ipgen
-        '';
-      };
-  };
+          installPhase = ''
+            install -Dm775 ./target/release/${name} $out/bin/${name}
+          '';
+        };
+    };
 }
